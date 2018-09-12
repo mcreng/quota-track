4c18271128f574770ad1102178d8b074864356e6
mcreng <tsehonam@gmail.com>
2018-09-12 23:40:00 +0800

    Added route for getting quota data

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fetchCourses = require("./fetch");
 const logger = require("./logger")("api");
+const jsonfile = require("jsonfile");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +13,17 @@ app.route("/api").get((req, res) => {
   res.status(200).send({ received: true });
 });
 
+/**
+ * GET
+ * @param subject - Subject Name
+ */
+app.route("/api/data/quota").get((req, res) => {
+  res
+    .status(200)
+    .sendFile(`./data/quota/${req.body.subject}.json`, { root: "." });
+});
+
 // Fetch once, then after so, fetch again per 60 minutes
-fetchCourses(1000).then(setInterval(() => fetchCourses(1000), 1000 * 60 * 60));
+// fetchCourses(1000).then(setInterval(() => fetchCourses(1000), 1000 * 60 * 60));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
